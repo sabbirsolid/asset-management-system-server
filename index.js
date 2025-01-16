95297185e3dcae4aca771a498acf2fc840cf4789
sabbirsolid <sabbir.designer21@gmail.com>
2025-01-16 14:24:43 +0600

    added an api for posting user

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Asset Management System is running");
 });
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.98vvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.98vvu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,8 +34,17 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-
-
+    const userCollection = client.db("assetManagementDB").collection("users");
+    // posting data to database
+    app.post("/users", async (req, res) => {
+      const query = { email: req.body.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send("User already in the database");
+      }
+      const result = await userCollection.insertOne(req.body);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
